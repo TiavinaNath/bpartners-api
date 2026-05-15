fa7effc4e733889a20c3f4407ba1291f334f0742
TiavinaNath <hei.tiavina.3@gmail.com>
2026-05-15 08:18:53 +0300

    chore: fix db true compat

--- src/main/java/app/bpartners/api/repository/DummyRepository.java
+++ src/main/java/app/bpartners/api/repository/DummyRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @PojaGenerated
-@SuppressWarnings("all")
 @Repository
 public interface DummyRepository extends JpaRepository<Dummy, String> {
 
