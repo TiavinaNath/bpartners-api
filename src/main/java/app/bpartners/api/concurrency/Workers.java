24d0ad9e32d27d66431cc85f97bf459db14b4a9f
TiavinaNath <hei.tiavina.3@gmail.com>
2026-06-11 10:47:39 +0300

    chore: refactor workers

--- src/main/java/app/bpartners/api/concurrency/Workers.java
+++ src/main/java/app/bpartners/api/concurrency/Workers.java
@@ -15,7 +15,6 @@ import lombok.SneakyThrows;
 import org.springframework.stereotype.Component;
 
 @PojaGenerated
-@SuppressWarnings("all")
 @Component
 public class Workers {
   private final ExecutorService executorService;
@@ -34,21 +33,19 @@ public class Workers {
                     (Callable<Void>)
                         () -> {
                           renameThread(
-                              currentThread(), getRandomSubThreadNamePrefixFrom(parentThread));
+                              parentThread, getRandomSubThreadNamePrefixFrom(parentThread));
                           return c.call();
                         })
             .toList();
-    // TODO: refactor properly
     List<Future<Void>> futures = executorService.invokeAll(callables);
-    return futures.stream()
-        .map(
-            future -> {
-              try {
-                return future.get();
-              } catch (InterruptedException | ExecutionException e) {
-                throw new RuntimeException(e);
-              }
-            })
-        .toList();
+    return futures.stream().map(this::handleFutureException).toList();
+  }
+
+  private Void handleFutureException(Future<Void> future) {
+    try {
+      return future.get();
+    } catch (InterruptedException | ExecutionException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
